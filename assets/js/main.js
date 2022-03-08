3b72159ce42e16a7459ae6e798f13590ff783689
Richardoxd22 <richard29san97@outlook.com>
2022-03-07 21:31:11 -0600

    Botones con secuencia de sección

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
-    const header =document.getElementById('header')
-
-    if(this.scrollY >=50) header.classList.add('scroll-header');else header
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
-//window.addEventListener('scroll, scrollHeader')
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 const modalViews = document.querySelectorAll('.services__modal'),
@@ -66,7 +66,24 @@ let swipertestimonial = new Swiper(".testimonial__container", {
     },
   });
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== LIGHT DARK THEME ===============*/ 
 
